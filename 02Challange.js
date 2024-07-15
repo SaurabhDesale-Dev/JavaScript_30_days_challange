388de612a9884548857e88d40d4837fba3e4fd4a
Saurabh <90861229+SaurabhDesale-Dev@users.noreply.github.com>
2024-07-15 10:59:30 +0530

    Add files via upload

---
+++ 02Challange.js
@@ -0,0 +1,36 @@
+// Activity 1: Arithmetic Operations
+
+console.log(2 + 2); // Task 1
+console.log(3 - 2); // Task 2
+console.log(4 * 4); // Task 3
+console.log(10 / 2); // Task 4
+console.log(15 % 2); // Task 5
+
+// Activity 2: Assignment Operators
+
+let number = 1;
+console.log((number += 1)); // Task 6
+console.log((number -= 2)); // Task 7
+
+// Activity 3: Comparison Operators
+
+console.log(3 > 2); // Task 8
+console.log(4 < 5); // Task 8
+console.log(10 >= 10); // Task 9
+console.log(20 <= 10); // Task 9
+console.log("2" == 2); // Task 10
+console.log("2" === 2); // Task 10
+
+// Activity 4: Logical operator
+const a = 10;
+const b = 20;
+
+console.log(a > 5 && b > 10); // Task 11
+
+console.log(a > 10 || b > 1); // Task 12
+
+console.log(a != 5); // Task 13
+
+// Activity 5:Ternary Operator
+const num = -2;
+console.log(num > 0 ? "positive" : "negative");
